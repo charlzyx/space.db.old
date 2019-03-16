f4b0367edecefc619c79c72bf9927ef8bd0f8bcb
charlzyx <yhxnic@qq.com>
2019-03-16 23:00:01 +0800

    change to use-immer but no debug

--- pkg/space.js
+++ pkg/space.js
@@ -2,11 +2,17 @@
 /**
  * Space/DataBinding
  */
-import React, { createContext, Component, useState } from 'react';
+import React, { createContext, Component } from 'react';
 import _ from 'lodash';
-import produce from 'immer';
+import { useImmer } from 'use-immer';
+import { createDraft, isDraft, finishDraft } from 'immer';
 
-// utils
+
+/**
+ * -----------------------------------------------------
+ * Helpers for Space
+ * -----------------------------------------------------
+ */
 const isThenable = p => typeof p.then === 'function';
 
 /**
@@ -14,8 +20,8 @@ const isThenable = p => typeof p.then === 'function';
  * - Path Resolver: ./../../ and / support
  */
 const pathResolve = (space, field) => {
-  const shouldDot = !!space;
-  return shouldDot ? `${space || ''}.${field || ''}` : field || '';
+  const shouldDot = space && field && (field[0] !== '[');
+  return shouldDot ? `${space || ''}.${field || ''}` : `${space || ''}${field || ''}`;
 };
 
 const findAwaiting = (path, map) => {
@@ -23,49 +29,83 @@ const findAwaiting = (path, map) => {
   return map[foundKey] || [];
 };
 
+/**
+ * -----------------------------------------------------
+ * Core Context
+ * -----------------------------------------------------
+ */
 const SpaceCtx = createContext({});
 
-const hole = {
-  store: null,
-  change: () => {
-    throw new Error('[Space/hole] error, you should not seen this message.');
-  },
-};
-
+/**
+ * -----------------------------------------------------
+ * just cache bridge for hole, private by closure.
+ * -----------------------------------------------------
+ */
 const wormhole = {
-  pull: space => _.get(hole.store, pathResolve(space)),
-  put: (space, producer) => {
-    const { store } = hole;
-    const next = produce(_.get(store, space), producer);
-    const nextStore = produce(store, (draft) => {
-      draft[space] = next;
-    });
-    hole.change(nextStore);
+  store: null,
+  put: () => {
+    throw new Error('[Space/wormhole] error, you should not seen this message.');
   },
 };
 
-Object.freeze(wormhole);
+/**
+ * -----------------------------------------------------
+ * getter and setter to export
+ * @function pull(space, ...fields) => any
+ * @type what notFunction | funcAsProducer
+ * @function put(space, field) => putWhat
+ * @function put(space, field, what) => next
+ * -----------------------------------------------------
+ */
+const hole = {
+  pull: (space, ...fields) => {
+    const path = pathResolve(space, fields.reduce((last, field) => pathResolve(last, field), ''));
+    _.get(wormhole.store, path);
+  },
+  put: (space, field, ...maybeWhat) => {
+    const path = pathResolve(space, field);
+    if (maybeWhat.length === 1) {
+      return wormhole.put((draft) => {
+        const what = maybeWhat[0];
+        const next = typeof what === 'function'
+          ? what(_.get(draft, path))
+          : what;
+        _.set(draft, path, next);
+      });
+    }
+    const putWhat = w => wormhole.put((draft) => {
+      const next = typeof w === 'function'
+        ? w(_.get(draft, path))
+        : w;
+      _.set(draft, path, next);
+    });
+    return putWhat;
+  },
+};
 
+// protected out side change
+Object.freeze(hole);
 
+/**
+ * -----------------------------------------------------
+ * core provider, provider store and put
+ * -----------------------------------------------------
+ */
 const SpaceProvider = (props) => {
-  const [store, change] = useState({
+  const [store, put] = useImmer({
     // 异步表
     __awaiting_map: {
       // [path]: ['pending' | null, Error | null] | null
     },
   });
 
-  // black hole
-  hole.store = store;
-  hole.change = change;
+  wormhole.store = store;
+  wormhole.put = put;
 
   const ctx = {
     space: null,
     store,
-    change: (next) => {
-      console.debug('[Space next]\n', next);
-      change(next);
-    },
+    put,
   };
 
   return (
@@ -75,23 +115,37 @@ const SpaceProvider = (props) => {
   );
 };
 
+/**
+ * -----------------------------------------------------
+ * MOST INTERESTING! CORE IDEA!
+ * manager for space name and init
+ * @props space unique string to namespaced module
+ * @props init the init value for this space
+ *
+ * here is the core code.
+ * <Ctx.Consumer>
+ *   <Ctx.Provider>
+ *   </Ctx.Provider>
+ * </Ctx.Consumer>
+ * -----------------------------------------------------
+ */
 class Space extends Component {
   renderSpace = (ctx) => {
     // TODO: init check and Map Consider how to reset
     const { space: nowSpace, children, init } = this.props;
-    const { space: parentSpace, store, change } = ctx;
+    const { space: parentSpace, store, put } = ctx;
 
     const space = pathResolve(parentSpace, nowSpace);
 
+    // TODO: thinking about life circle
     const shouldInit = init && _.get(store, space) === undefined;
 
     const nextCtx = { ...ctx, space };
 
     if (shouldInit) {
-      nextCtx.store = produce(store, (draft) => {
+      put((draft) => {
         _.set(draft, space, init);
       });
-      change(nextCtx.store);
     }
 
     return (
@@ -110,13 +164,22 @@ class Space extends Component {
   }
 }
 
+
+/**
+ * field: string
+ * async: bool| string
+ * computed: [[need], computer: () => {}]
+ */
 class Ship extends Component {
   renderShip = (ctx) => {
     const {
-      space, store, change,
+      space, store, put,
     } = ctx;
 
-    const { children, field, await: awaiting } = this.props;
+    const {
+      children, field, await: awaiting,
+    } = this.props;
+    // TODO: props check
     if (!children) {
       console.warn('[space/Ship] should have an children');
       return null;
@@ -127,41 +190,45 @@ class Ship extends Component {
     const path = pathResolve(space, field);
     // await: string | bool
     if (awaiting) {
-      onChange = (next) => {
+      onChange = (v) => {
+        const next = isDraft(v) ? finishDraft(v) : v;
+
         if (isThenable(next)) {
-          // update await map
-          change(produce(store, (draft) => {
+          // update awaiting map
+          put((draft) => {
             draft.__awaiting_map[awaitingPath] = ['pending'];
-          }));
+          });
 
           // then
           next.then((awaitNext) => {
-            change(produce(store, (draft) => {
+            put((draft) => {
               _.set(draft, awaitingPath, awaitNext);
               draft.__awaiting_map[awaitingPath] = [];
-            }));
+            });
           }).catch((e) => {
-            change(produce(store, (draft) => {
+            put((draft) => {
               draft.__awaiting_map[awaitingPath] = [null, e];
-            }));
+            });
           });
         } else { // sync
-          change(produce(store, (draft) => {
+          put((draft) => {
             _.set(draft, awaitingPath, next);
-          }));
+          });
         }
       };
     } else { // sync
-      onChange = (next) => {
-        change(produce(store, (draft) => {
+      onChange = (v) => {
+        const next = isDraft(v) ? finishDraft(v) : v;
+
+        put((draft) => {
           _.set(draft, path, next);
-        }));
+        });
       };
     }
 
     const childProps = {
       ...children.props,
-      value: _.get(store, path),
+      value: createDraft(_.get(store, path)),
       onChange,
       awaiting: findAwaiting(path, store.__awaiting_map),
     };
@@ -182,5 +249,5 @@ export {
   SpaceProvider,
   Space,
   Ship,
-  wormhole as space,
+  hole as space,
 };
---
+++ src/immerApp.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useImmer } from 'use-immer';
+import _ from 'lodash';
+
+
+function App() {
+  const [person, updatePerson] = useImmer({
+    name: 'Michel',
+    age: 33,
+  });
+
+  function updateName(name) {
+    updatePerson((draft) => {
+      _.set(draft, 'a.b.c', name);
+    });
+  }
+
+  function becomeOlder() {
+    updatePerson((draft) => {
+      _.set(draft, 'e.b.c', +new Date());
+    });
+  }
+
+  return (
+    <div className="App">
+      <pre>
+        {JSON.stringify(person, null, 2)}
+      </pre>
+      <h1>
+        Hello
+        {person.name}
+        {person.age}
+      </h1>
+      <hr/>
+      <div>
+        {_.get(person, 'a.b.c')}
+        {_.get(person, 'e.b.c')}
+      </div>
+      <hr/>
+      <input
+        onChange={(e) => {
+          updateName(e.target.value);
+        }}
+        value={person.name}
+      />
+      <br />
+      <button onClick={becomeOlder}>Older</button>
+    </div>
+  );
+}
+
+export default App;
